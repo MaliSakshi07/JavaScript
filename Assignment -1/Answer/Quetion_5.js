2cc1145e7d242b85a7d5deba3ac89b59233b0a85
MaliSakshi07 <sakshimali467@gmail.com>
2025-09-26 21:18:56 +0530

    Assignment

---
+++ Assignment -1/Answer/Quetion_5.js
@@ -0,0 +1,13 @@
+//Accept one number from user and print that number of * on screen.
+
+function Accept(iNo)
+{
+ iCnt = 0;
+for( iCnt = 1 ; iCnt <= iNo ; iCnt++)
+{
+console.log("*");
+}
+}
+let iValue = 0;
+iValue = 5;
+Accept(iValue);
