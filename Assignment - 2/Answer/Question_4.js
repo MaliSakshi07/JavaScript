9f9edfbd23f42871ba1d69dac9868c9442451cd7
MaliSakshi07 <sakshimali467@gmail.com>
2025-10-03 21:47:29 +0530

    Assignment

---
+++ Assignment - 2/Answer/Question_4.js
@@ -0,0 +1,13 @@
+//Accept two numbers from user and display first number in second number of times.
+
+function Display(iNo, iFrequency)
+{
+    let i = 0;
+
+    for(i=iFrequency; i>0;--i)
+    {
+        console.log(iNo);
+    }
+}
+let Num = prompt("Enter a Number :");
+let Freq = prompt("Enter Frequency :");
